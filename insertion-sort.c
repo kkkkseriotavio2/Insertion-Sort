3d528d5a4603315f86b58ca0c4205948ba8c680a
kkkkseriotavio2 <105679388+kkkkseriotavio2@users.noreply.github.com>
2023-12-15 12:13:30 -0300

    Create insertion-sort.c

---
+++ insertion-sort.c
@@ -0,0 +1,28 @@
+//  Algoritmo de Ordenação por Inserção
+#include <stdio.h>
+
+int v[] = {35, 8, 5, 1, 21, 1, 2, 3, 12};
+
+int main(void){
+
+    for (int j = 1; j < 9; j++){
+        int t = v[j];
+        int i = j - 1;
+        while (i > -1 && v[i] > t){
+            v[i+1] = v[i];
+            i = i - 1;
+        }
+        v[i+1] = t;
+        int n = 0;
+        do{
+            if(n < 8){
+                printf("%d, ", v[n]);
+            }
+            else{
+                printf("%d.\n", v[n]);
+            }
+            n++;
+        }while (n != 9);
+    }
+    return 0;
+}
